c32fe989a1f7ff2121bd984be7a1cbecf71ae364
MikemoriKeisuke <mikekit41@gmail.com>
2016-12-01 00:18:14 +0900

    プロジェクト ファイルを追加します。

---
+++ sushi3/sushi3/App.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace sushi3
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new MyPage();
+        }
+
+        class MyPage : ContentPage
+        {
+            public MyPage()
+            {
+                Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+                var source = new HtmlWebViewSource
+                {
+                    Html = "<marquee><font size='5'>🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　🍣　　</font></marquee>"
+                };
+
+                Content = new WebView
+                {
+                    Source = source
+                };
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
